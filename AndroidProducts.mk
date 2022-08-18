c86d26e450818818782690ada704824ad986b3d9
gorkemoji <emircan9921@outlook.com>
2022-08-18 18:28:34 +0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,6 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/full_zerolteusc.mk \
+    $(LOCAL_DIR)/aokp.mk \
+    $(LOCAL_DIR)/aosp_zerolteusc.mk \
+    $(LOCAL_DIR)/lineage.mk \
+    $(LOCAL_DIR)/omni_zerolteusc.mk
