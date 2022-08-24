0c8d975ec9b4f6d9f7480ed73c94e8c53b6597d4
gorkemoji <emircan9921@outlook.com>
2022-08-24 17:58:45 +0300

    Add files via upload

---
+++ init/init_sec.cpp
@@ -0,0 +1,78 @@
+/*
+   Copyright (c) 2015, The Dokdo Project. All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+   File Name : init_sec.c
+   Create Date : 2015.11.03
+   Author : Sunghun Ra
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "log.h"
+#include "property_service.h"
+#include "util.h"
+#include "vendor_init.h"
+
+#include "init_sec.h"
+
+std::string bootloader;
+std::string device;
+char* devicename;
+
+device_variant check_device_and_get_variant()
+{
+    std::string platform = property_get("ro.board.platform");
+    if (platform != ANDROID_TARGET) {
+        return UNKNOWN;
+    }
+
+    bootloader = property_get("ro.bootloader");
+    return match(bootloader);
+}
+
+void vendor_load_properties()
+{
+    device_variant variant = check_device_and_get_variant();
+
+    switch (variant) {
+        case G925R4:
+           /* zerolteusc */
+           property_set("ro.build.fingerprint", "samsung/zerolteusc/zerolteusc:7.0/NRD90M/G925R4TYU4DRI1:user/release-keys");
+           property_set("ro.build.description", "zerolteusc-user 7.0 NRD90M G925R4TYU4DRI1 release-keys");
+           property_set("ro.product.model", "SM-G925R4");
+           property_set("ro.product.device", "zerolteusc");
+           break;
+        default:
+            ERROR("Unknown bootloader id %s detected. bailing...\n", bootloader.c_str());
+            return;
+    }
+    device = property_get("ro.product.device");
+    INFO("Found bootloader id %s setting build properties for %s device\n", bootloader.c_str(), device.c_str());
+}
