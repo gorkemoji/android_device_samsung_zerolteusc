44d5250a2e1136311d096d15ee7479f81c6435da
gorkemoji <emircan9921@outlook.com>
2022-08-29 21:15:02 +0300

    Delete AndroidProducts.mk

--- AndroidProducts.mk
+++
@@ -1,6 +0,0 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_zerolteusc.mk \
-    $(LOCAL_DIR)/aokp.mk \
-    $(LOCAL_DIR)/aosp_zerolteusc.mk \
-    $(LOCAL_DIR)/lineage.mk \
-    $(LOCAL_DIR)/omni_zerolteusc.mk
